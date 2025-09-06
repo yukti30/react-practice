bf7ead9a685ba95fba14c642e97f6d789fb37e88
YUKTI GUPTA <yuktigupta2012@gmail.com>
2025-09-06 13:23:19 +0530

    Build Responsive UI

--- CapstopeProject/VideogameApp/src/App.tsx
+++ CapstopeProject/VideogameApp/src/App.tsx
@@ -1,8 +1,31 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
-      <Button>Click me</Button>{" "}
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`, // width > 1024 px
+        }}
+      >
+        <GridItem area="nav" bg="blue">
+          Nav
+        </GridItem>
+
+        {/* Show only on large screens */}
+        <GridItem
+          area="aside"
+          bg="yellow"
+          display={{ base: "none", lg: "block" }}
+        >
+          Aside
+        </GridItem>
+
+        <GridItem area="main" bg="red">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
--- CapstopeProject/VideogameApp/src/main.tsx
+++ CapstopeProject/VideogameApp/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "@/components/ui/provider";
 // import './index.css'
 import App from "./App.tsx";
-import { Provider } from "@/components/ui/provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
