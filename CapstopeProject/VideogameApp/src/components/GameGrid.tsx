c0a0a7385ef4d5a7da762d92ae950832e123e0c2
YUKTI GUPTA <yuktigupta2012@gmail.com>
2025-09-08 20:09:56 +0530

    optimised images

--- CapstopeProject/VideogameApp/src/components/GameGrid.tsx
+++ CapstopeProject/VideogameApp/src/components/GameGrid.tsx
@@ -7,7 +7,11 @@ const GameGrid = () => {
   return (
     <>
       {errors && <Text>{errors}</Text>}
-      <SimpleGrid columns={{ sm: 1, lg: 3, xl: 5 }} padding="10px">
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+        padding="10px"
+      >
         {games.map((game) => (
           <GameCard key={game.id} game={game}></GameCard>
         ))}
