3adfa3b2d46003d1096530bf5b4bdfb3421a210a
YUKTI GUPTA <yuktigupta2012@gmail.com>
2025-09-13 12:38:55 +0530

    Cleaned game card

--- CapstopeProject/VideogameApp/src/components/GameGrid.tsx
+++ CapstopeProject/VideogameApp/src/components/GameGrid.tsx
@@ -16,8 +16,8 @@ const GameGrid = ({ gameQuery }: Props) => {
     <>
       {errors && <Text>{errors}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={3}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={6}
         padding="10px"
       >
         {isLoading &&
