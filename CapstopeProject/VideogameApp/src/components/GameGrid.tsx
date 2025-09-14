7eb6db27d46266e91d684ba70c952f9a3d5ec43b
YUKTI GUPTA <yuktigupta2012@gmail.com>
2025-09-14 11:19:03 +0530

    added refactoring of game grid

--- CapstopeProject/VideogameApp/src/components/GameGrid.tsx
+++ CapstopeProject/VideogameApp/src/components/GameGrid.tsx
@@ -12,27 +12,25 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, errors, isLoading } = useGame(gameQuery);
   const Skeletons = [1, 2, 3, 4, 5, 6];
+  if (errors) <Text>{errors}</Text>;
   return (
-    <>
-      {errors && <Text>{errors}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
-        padding="10px"
-      >
-        {isLoading &&
-          Skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton></GameCardSkeleton>
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game}></GameCard>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+      padding="10px"
+    >
+      {isLoading &&
+        Skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton></GameCardSkeleton>
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game}></GameCard>
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
