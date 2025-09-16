107bcccad5b5f4df9327cea6e725f81ff78c7d84
YUKTI GUPTA <yuktigupta2012@gmail.com>
2025-09-17 01:08:06 +0530

    added Personal Details

--- Project8/Multi Step Registration Form/src/App.tsx
+++ Project8/Multi Step Registration Form/src/App.tsx
@@ -1,9 +1,11 @@
+import PersonalDetails from "./components/PersonalDetails";
 import ProgressSlider from "./components/ProgressSlider";
 
 const App = () => {
   return (
     <>
       <ProgressSlider></ProgressSlider>
+      <PersonalDetails></PersonalDetails>
     </>
   );
 };
--- Project8/Multi Step Registration Form/src/components/PersonalDetails.tsx
+++ Project8/Multi Step Registration Form/src/components/PersonalDetails.tsx
@@ -1,5 +1,66 @@
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button,
+  HStack,
+} from "@chakra-ui/react";
+import { useForm, type FieldValues } from "react-hook-form";
+
+interface PersonalDetails {
+  name: string;
+  age: number;
+  email: string;
+}
 const PersonalDetails = () => {
-  return <div>PersonalDetails</div>;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isValid },
+  } = useForm<PersonalDetails>();
+  return (
+    <>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <FormControl padding={6} isInvalid={!!errors.name}>
+          <FormLabel>Full Name</FormLabel>
+          <Input {...register("name", { required: true })} type="text" />
+          {errors.name && (
+            <FormErrorMessage>Field is Required</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl padding={6} isInvalid={!!errors.age}>
+          <FormLabel>How old are you?</FormLabel>
+          <Input
+            {...register("age", {
+              required: "Age is required",
+              min: { value: 18, message: "You must be at least 18" },
+            })}
+            type="number"
+          />
+          {errors.age && (
+            <FormErrorMessage>{errors.age.message}</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl padding={6} isInvalid={!!errors.email}>
+          <FormLabel>Email address</FormLabel>
+          <Input {...register("email", { required: true })} type="email" />
+          {errors.email && (
+            <FormErrorMessage>Field is Required</FormErrorMessage>
+          )}
+        </FormControl>
+        <HStack justifyContent="space-between" padding={6}>
+          <Button colorScheme="teal" size="sm" type="button" disabled={true}>
+            Previous
+          </Button>
+          <Button colorScheme="teal" size="sm" type="submit">
+            Next
+          </Button>
+        </HStack>
+      </form>
+    </>
+  );
 };
 
 export default PersonalDetails;
